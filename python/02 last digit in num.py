bc513aca6626ba49927eb69405e8396c4515af8a
MUKESH333RS <143300934+MUKESH333RS@users.noreply.github.com>
2024-07-16 19:39:44 +0530

    02 last digit in num.py

--- python/02 last digit in num.py
+++ python/02 last digit in num.py
@@ -14,3 +14,16 @@ print("The last digit of",num,"is",findlastdigit(num,length))
 # input = 12345
 # first digit = 1
 # second digit = 5
+
+# 2nd method
+
+def findfirstdigit(n):
+    while n >= 10:
+        n = n / 10
+    return int(n)
+def findlastdigit(n):
+    return (n % 10)
+
+num = int(input("Enter a numbers:"))
+print("The first digit of",num,"is",findfirstdigit(num))
+print("The last digit of",num,"is",findlastdigit(num))
