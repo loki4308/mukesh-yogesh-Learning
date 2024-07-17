ecbb18f5fee385d09b75a3285dc9a99c1173b196
MUKESH333RS <143300934+MUKESH333RS@users.noreply.github.com>
2024-07-17 15:19:55 +0530

    Add files via upload

---
+++ python/08 divisors of a number.py
@@ -0,0 +1,14 @@
+# Print all divisors of a number
+
+def divisors_of_num(n):
+    divisors = []
+    for i in range(1,(n // 2)+1,1):
+        if n % i == 0:
+            divisors.append(i)
+    divisors.append(n)
+    return divisors
+
+
+num = int(input("Enter a number:"))
+result = divisors_of_num(num)
+print(result)
